4a3369250d014ba2b611c7e1aceda13f56869488
維菘 <weisung@weisongde-mbp.iiiedu.local>
2020-10-14 18:07:07 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState} from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([])
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
