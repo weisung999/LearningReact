206f310f0d47a3e580ce4b1dec32bb4d2415bd18
維菘 <weisung@weisongde-MacBook-Pro.local>
2020-10-20 17:33:50 +0800

    uuid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState} from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value  // value of input
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete:false}]
+    })
+    todoNameRef.current.value = null //輸入完清除
+  }
+
   return (
     <>
-      <TodoList />
-      <input type="text" />
-      <button>Add Todo</button>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to do</div>
     </>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Todo from './Todo'
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
     return (
-        <div>
-            Hello 
-        </div>
+        todos.map(todo => {
+            return <Todo key={todo.id} todo={todo} />
+        })
     )
 }
